c08b30a0c129744372f43ea2a309bcac63536357
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2021-01-24 23:18:21 +0900

    Update count.py

--- scripts/count.py
+++ scripts/count.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
   
-   import rospy
-   from std_msgs.msg import Int32
+  import rospy
+  from std_msgs.msg import Int32
   
   rospy.init_node('count')
   pub = rospy.Publisher('count_up', Int32, queue_size=1)
@@ -9,6 +9,6 @@
   n = 0
  
  while not rospy.is_shutdown():
- n += 1
- pub.publish(n);
- rate.sleep()
+   n += 1
+   pub.publish(n);
+   rate.sleep()
