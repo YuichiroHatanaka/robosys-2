2ac1b18af8496439df20e599908aab48d8879e69
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2021-01-24 23:21:04 +0900

    Update twice.py

--- scripts/twice.py
+++ scripts/twice.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import rospy
-  4 from std_msgs.msg import Int32
-  5
-  6 def cb(message):
-  7         rospy.loginfo(message.data*2)
-  8
-  9 if __name__ == '__main__':
- 10         rospy.init_node('twice')
- 11         sub = rospy.Subscriber('count_up', Int32, cb)
- 12         rospy.spin()
- 1
+from std_msgs.msg import Int32
+
+def cb(message):
+  rospy.loginfo(message.data*2)
+ 
+if __name__ == '__main__':
+  rospy.init_node('twice')
+  sub = rospy.Subscriber('count_up', Int32, cb)
+  rospy.spin()
