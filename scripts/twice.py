c6beb0b9574261564899c8a0ce797e331a0c2ad3
Yuichiro Hatanaka <s19C1092el@s.chibakoudai.jp>
2021-01-24 14:25:29 +0000

    add a file

--- scripts/twice.py
+++ scripts/twice.py
@@ -4,9 +4,9 @@ import rospy
 from std_msgs.msg import Int32
 
 def cb(message):
-  rospy.loginfo(message.data*2)
+	rospy.loginfo(message.data*2)
  
 if __name__ == '__main__':
-  rospy.init_node('twice')
-  sub = rospy.Subscriber('count_up', Int32, cb)
-  rospy.spin()
+	rospy.init_node('twice')
+	sub = rospy.Subscriber('count_up', Int32, cb)
+	rospy.spin()
