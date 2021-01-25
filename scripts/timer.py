b4ed9a6dfc4cf005a0b07931770b9f2cf895aea7
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2021-01-25 21:33:26 +0900

    Update timer.py

--- scripts/timer.py
+++ scripts/timer.py
@@ -15,14 +15,22 @@ while not rospy.is_shutdown():
     figure = int(input())
 
     if figure == 1:
+        timer_time = 180
         start_time = rospy.get_time()
-        print("timer start")
+        print("180s timer start")
+        finish_time = start_time + timertime
+        
+        for(;;)
+        if rospy.get_time == finishtime:
+        break
+        
+        printf("時間です！")
 
     if figure == 2:
-        finish_time = rospy.get_time()
+        stop_time = rospy.get_time()
         print("timer stop")
-        time = finish_time - start_time
-        print(time)
+        time = timer_time + start_time - finish_time 
+        print("あと"time"です")
         pub.publish(time)
 
 
